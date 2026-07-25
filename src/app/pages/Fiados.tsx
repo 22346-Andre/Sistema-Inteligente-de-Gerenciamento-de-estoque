479510dbbf1c647ad75ffdf2495ae098ba1622d4
André_Lucas <134009910+22346-Andre@users.noreply.github.com>
2026-07-25 16:04:03 -0300

    correção de erros

--- src/app/pages/Fiados.tsx
+++ src/app/pages/Fiados.tsx
@@ -10,7 +10,7 @@ import { toast } from 'sonner';
 import { format, parseISO, isValid } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { fiadoService, ContaReceber } from '../services/fiado.service';
-import { pixService } from '../services/pix.service';
+import { pixService } from '../services/pix.Service';
 import { PixCobrancaDialog } from '../components/PixCobrancaDialog';
 
 // Formata uma data com segurança: se vier vazia/nula ou inválida do backend,
